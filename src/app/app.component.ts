90bbd2004db592b87f4776dae6aa102e3bf60255
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-05-18 15:19:22 +0530

    #SB-26905 feat: updated with PR comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,18 +51,21 @@ export class AppComponent {
       transcripts: [
         {
           language: 'Bengali',
+          languageCode: 'bn',
           identifier: 'do_11353887890259968011412',
           // tslint:disable-next-line:max-line-length
           artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11353887890259968011412/titanic.1997.3d.720p.bluray.x264.-yts.mx-english.srt'
         },
         {
           language: 'English',
+          languageCode: 'en',
           identifier: 'do_11353887890285363211413',
           // tslint:disable-next-line:max-line-length
           artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11353887890285363211413/titanic.1997.3d.720p.bluray.x264.-yts.mx-english.srt'
         },
         {
           language: 'Assamese',
+          languageCode: 'as',
           identifier: 'do_11353887890287820811414',
           // tslint:disable-next-line:max-line-length
           artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11353887890287820811414/titanic.1997.3d.720p.bluray.x264.-yts.mx-english.srt'
